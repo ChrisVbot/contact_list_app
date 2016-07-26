56eb69fe756938e6cf43cabc7de94127ed29cc46
ChrisVbot <chris.veillette@gmail.com>
2016-07-26 21:16:20 +0000

    need to add argv functionality

--- contact_list.rb
+++ contact_list.rb
@@ -1,32 +1,46 @@
 require_relative 'contact'
 
+unless ARGV[0] == true
 puts "Here is a list of available commands:
   new - Create a new contact
   list - list all contacts
   show - show a contact
   search - search all contacts"
+end
 
-@input = ARGV.first
-# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
   def initialize(input)
     @input = input
     input = gets.strip
     case input
       when 'new'
-        #do something
+        #runs the new method in Contact class to add to contacts.csv
+        puts 'Enter name: '
+        name = gets.chomp
+        puts 'Enter email: '
+        email = gets.chomp
+        puts Contact.create(name, email)
+        puts "#{name} has been added to the database."
       when 'list'
-        #do something
+        #runs the all method in Contact class
+        puts Contact.all
       when 'show'
-        #do something
-      when 'search'
+        #runs the find method in Contact class
+        puts 'Enter ID of contact to display: '
+        id = gets.chomp.capitalize
+        puts Contact.find(id)
+      when 'search' 
+        #runs the search method in Contact class
+        puts "Enter search term: "
+        s = gets.chomp
+        puts Contact.search(s)
       else 
           'Command not found'
-        end
-        #do something
+      end  
   end
 
     
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
 end
+ContactList.new(ARGV.first)
