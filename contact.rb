fd6dcf0591981fce3aa85b96c8e74457d448a127
ChrisVbot <chris.veillette@gmail.com>
2016-08-03 04:09:05 +0000

    minor updates to comments for self-clarity

--- contact.rb
+++ contact.rb
@@ -7,21 +7,19 @@ class Contact
   attr_accessor :name, :email
   attr_reader :id
   
-  # Creates a new contact object
-
+  #id=nil because we allow the database to create one for us when creating new contact. Otherwise ID is used to check if DB entry exists for that individual. 
   def initialize(name, email, id=nil) 
     @name = name
     @email = email
     @id = id
   end
 
-  #Overrides default to_s method to display output as strings instead of 
-  # SQL objects. 
+  #Overrides default Ruby to_s method to display output as strings instead of SQL objects. 
   def to_s
     "#{id}: #{name} (#{email})"
   end
 
-  #Saves instanced contact into 'contacts' database
+  #Saves instanced contact into 'contacts' database. If contact already exists (verified by id), instead updates that contact's information. 
   def save
     if id 
       Contact.connection.exec_params('UPDATE contacts SET name = $1, email = $2 WHERE id = $3;', [name, email, id])
@@ -30,11 +28,12 @@ class Contact
     end
   end
 
-  #Deletes the instanced contact from 'contacts' database
+  #Deletes the contact from 'contacts' database
   def destroy
     Contact.connection.exec_params('DELETE FROM contacts WHERE id = $1;', [id])
   end
-  #Everything below this is a class method. 
+
+  #Everything below this line is a class method. 
   class << self
     
     def connection
@@ -59,18 +58,18 @@ class Contact
       Contact.new(name, email)
     end
     
-    # Find the Contact in the 'contacts' database with the matching id.
+    # Find the Contact in the 'contacts' database with the matching id and displays results via the result.ntuples function. This will allow only one row to popular in the output. 
     def find(id)    
       results = self.connection.exec_params('SELECT * FROM contacts WHERE id = $1',[id])
       if results.ntuples > 0
         row = results[0]
         self.new(row['name'], row['email'], row['id'])
       end
-      #Note: the code below works but is not what the question intended. 
+      #Note: the code below works but is not what the question intended as it allows for multiple rows. 
       # results = results.map{|row| self.new(row['name'], row['email'], row['id'])}
     end
     
-    #Finds the contact in the 'contacts' database with matching name.
+    #Finds the contact in the 'contacts' database with matching name. If multiple matches, returns multiples (hence why not using results.ntuples method as on find(id) method).
     def search(name)
       results = self.connection.exec_params('SELECT * FROM contacts WHERE name LIKE $1 ORDER BY id',["%#{name}%"])
       results = results.map{|row| self.new(row['name'], row['email'], row['id'])}
